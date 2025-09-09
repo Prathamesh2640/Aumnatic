1e38e51e787e739ee3d0c271d289705edd53b7d5
Prathamesh2640 <prathameshsharma1694@gmail.com>
2025-09-09 17:47:04 +0530

    Created an empty  and its corresponding  for future camera implementation

---
+++ app/src/main/java/com/project/cameraapp/CameraActivity.java
@@ -0,0 +1,14 @@
+package com.project.cameraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
